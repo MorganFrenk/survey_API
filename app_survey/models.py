e5b34295b011430f3dee3afe5c37258fae05e9d6
MorganFrenk <morganfrenk2@gmail.com>
2021-08-12 00:28:55 +0300

    Fix models; Survey date format fix and now not editable; Add date to Answer

--- app_survey/models.py
+++ app_survey/models.py
@@ -4,9 +4,13 @@ from django.utils import timezone
 
 
 class Survey(models.Model):
-    creator_user_id = models.ForeignKey(User, related_name='created_survey', on_delete=models.CASCADE)
+    creator_user_id = models.ForeignKey(
+        User,
+        related_name='created_survey',
+        on_delete=models.CASCADE,
+    )
     name = models.CharField(max_length=100)
-    date = models.DateTimeField(default=timezone.now())
+    date = models.DateTimeField(editable=False, default=timezone.now)
     description = models.CharField(max_length=200)
 
     def __str__(self):
@@ -14,15 +18,23 @@ class Survey(models.Model):
 
 
 class Question(models.Model):
-    survey = models.ForeignKey(Survey, related_name='questions', on_delete=models.CASCADE)
+    survey = models.ForeignKey(
+        Survey,
+        related_name='questions',
+        on_delete=models.CASCADE,
+    )
     text = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return self.text
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
+    question = models.ForeignKey(
+        Question,
+        related_name='choices',
+        on_delete=models.CASCADE
+    )
     text = models.CharField(max_length=200)
 
     def __str__(self):
@@ -30,10 +42,27 @@ class Choice(models.Model):
 
 
 class Answer(models.Model):
-    user_id = models.ForeignKey(User, related_name='answers', on_delete=models.CASCADE)
-    survey = models.ForeignKey(Survey, related_name='answers', on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
-    choice = models.ForeignKey(Choice, related_name='answers', on_delete=models.CASCADE)
+    user_id = models.ForeignKey(
+        User,
+        related_name='answers',
+        on_delete=models.CASCADE,
+    )
+    date = models.DateTimeField(editable=False, default=timezone.now)
+    survey = models.ForeignKey(
+        Survey,
+        related_name='answers',
+        on_delete=models.CASCADE,
+    )
+    question = models.ForeignKey(
+        Question,
+        related_name='answers',
+        on_delete=models.CASCADE,
+    )
+    choice = models.ForeignKey(
+        Choice,
+        related_name='answers',
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.choice.text
