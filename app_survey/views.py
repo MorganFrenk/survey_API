53a9a6f97017ce5edb24d31472b5b52201736d1d
MorganFrenk <morganfrenk2@gmail.com>
2021-08-12 00:16:50 +0300

    Add statistics for answers

--- app_survey/views.py
+++ app_survey/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 from app_survey.models import Answer, Choice, Question, Survey
 from app_survey.serializers import (
@@ -12,6 +15,13 @@ from app_survey.serializers import (
 class SurveyViewSet(viewsets.ModelViewSet):
     queryset = Survey.objects.all()
     serializer_class = SurveySerializer
+
+    @action(detail=True)
+    def statistics(self, request, pk=None, renderer_classes=[JSONRenderer]):
+        count = Answer.objects.filter(
+            survey=Survey.objects.filter(id=pk).get()
+        ).count()
+        return Response(count)
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
