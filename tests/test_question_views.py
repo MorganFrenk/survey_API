70edb9c68b2e2387b3df1b44fe9a692dc985664b
MorganFrenk <morganfrenk2@gmail.com>
2021-08-15 10:57:18 +0300

    Add list and create tests for questions

---
+++ tests/test_question_views.py
@@ -0,0 +1,53 @@
+import json
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from app_survey.models import Question, Survey
+
+
+class QuestionsTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass',
+        )
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.survey = Survey.objects.create(
+            user_id=self.user,
+            name='survey-name',
+            description='description',
+        )
+        self.question = Question.objects.create(
+            text='q-text',
+            user_id=self.user,
+            survey_id=self.survey.id,
+        )
+        self.random_user = User.objects.create_user(
+            username='randomuser',
+            password='randompass',
+        )
+        self.questions_url = f'{reverse("survey-list")}{self.survey.id}/questions/'
+
+    def test_questions_list_authed(self):
+        response = self.client.get(self.questions_url)
+        assert response.status_code == status.HTTP_200_OK
+        assert 'q-text' in json.loads(response.content)[0]['text']
+
+    def test_question_create_authed(self):
+        response = self.client.post(
+            self.questions_url,
+            data={
+                'text': 'new-q-text',
+            },
+            format='json',
+        )
+        
+        assert response.status_code == status.HTTP_201_CREATED
+        
